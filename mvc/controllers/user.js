f2b00fd7be64204a230213d3e402740e5780fb6a
Giuseppe D'Aquanno <33717671+GiuseppeDaquanno@users.noreply.github.com>
2017-11-16 22:26:39 +0100

    Update user.js

--- mvc/controllers/user.js
+++ mvc/controllers/user.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var router = express.Router()
-var db = require("../../db")
+var db = require("../db")
 
 //Attraverso la post su localhost/user viene chiamata la funzione save(che in teoria andrebbe gestita dal modello, ma che non ho
 //ancora visto
