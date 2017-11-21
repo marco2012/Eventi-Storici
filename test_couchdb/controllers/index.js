aca70306ebe6c3de131a67987548db9adfc590ef
marco2012 <marco.fox.marco@gmail.com>
2017-11-21 10:12:38 +0100

    removed folder

--- test_couchdb/controllers/index.js
+++
@@ -1,7 +0,0 @@
-var express = require("express")
-var router = express.Router()
-
-//Questo file è il controller che smista il controllo dei vari router definiti all'interno della cartella
-router.use("/comments", require("./comments"))
-
-module.exports = router
