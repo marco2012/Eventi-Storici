48e24856e6dc8994af24be0b9fdd207aa536dc15
Giuseppe D'Aquanno <33717671+GiuseppeDaquanno@users.noreply.github.com>
2017-11-22 09:11:06 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ var request = require("request")
 //URL del database(provvisoriamente in localhost)
 var db_url = "http://localhost:5984/progetto_rc/"
 //URL richiesta da CouchDB per eseguire la query
-var db_query = "http://localhost:5984/progetto_rc/_design/date/_view/getByDate"
+var view_url = "http://localhost:5984/progetto_rc/_design/date/_view/getByDate"
 
 
 //Metodo che attraverso POST http inserisce i documenti all'interno del database.
@@ -17,13 +17,10 @@ exports.save(doc, callback) {
         body: doc,
         json: true
     }, function(err, res, body) {
-        if (err) {
-            console.log("Errore durante l'inserimento di un documento nel database")
-            callback(err)
-        }
+        if (err) callback("Unable to connect")
         else {
-            console.log("Documento correttamente inserito nel database")
-            callback(null)
+            if (body.ok) callback(null)
+            else callback(body.reason)
         }
     })
 }
@@ -36,16 +33,12 @@ exports.save(doc, callback) {
 exports.fetch(date, callback) {
     var query_string = "?key=%22" + date.replace(/ /g, "+") + "%22"
     request.get({
-        url: db_query + query_string
+        url: view_url + query_string
     }, function(err, res, body) {
-        if (err) {
-            console.log("Documento non presente all'interno del database")
-            callback(err, null)
-        }
+        if (err) callback("Unable to connect", null)
         else {
-            console.log("Documento trovato nel database")
-            callback(null, body.value)
+            if (body.rows) callback(null, body.rows.value)
+            else callback("Document not found", null)
         }
     })
 }
-
