824eff05b918d78915f7092c403e8351ce08dd6c
marco2012 <marco.fox.marco@gmail.com>
2017-11-22 18:20:06 +0100

    Beh dai, CAPOLAVORO...

---
+++ ComeCazzoFunziona.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var app = express();
+
+var options = {
+  url: 'http://history.muffinlabs.com/date/2/14'
+}
+var anno;
+var testo;
+var cod;
+var coll;
+
+
+var data = {
+
+    "event" [
+
+        {   "year" : anno
+            "text" : testo
+            "html" : cod
+            "links" : coll
+        }
+    ]
+}
+
+app.get('February 14', function(req, res){
+    res.send(data.event.value);
+})
+
+app.get('options', function(req,res){
+  var temp = data.event.year;
+  console.log(temp);
+});
