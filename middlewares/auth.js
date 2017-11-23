063cea4ae586836901c1ac98c29e53c2faa0df02
GiuseppeDaquanno <GiuseppeDaquanno@outlook.com>
2017-11-23 17:14:25 +0100

    Maggior parte dei file da completare

---
+++ middlewares/auth.js
@@ -0,0 +1,4 @@
+//Marco questo è il tuo file, io di Oauth ancora non so niente quindi se mai comincia a pensare a come
+//si può integrare questo file con il controller
+
+exports.authenticate = function() {}
---
+++ middlewares/drive.js
@@ -0,0 +1,3 @@
+//In questo file gestiamo tutto ciò che è relativo a Google Drive.
+
+exports.write = function() {}
