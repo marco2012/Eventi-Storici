8c2210dc36513e9ab4cc35c443e3277eeafd6430
marco2012 <marco.fox.marco@gmail.com>
2017-11-23 13:25:30 +0100

    microsoft riconosce immagini

--- MicrosoftAI.js
+++ MicrosoftAI.js
@@ -3,19 +3,18 @@ var request = require("request")
 //https://westcentralus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fa/console
 
 var SUBSCRIPTION_KEY = "aecdcf236e59452499b41a84a8a56849"
-var SUBSCRIPTION_REGION = "westcentralus"
 
-var imageUrl = "https://blogs-images.forbes.com/erikkain/files/2017/02/Vikings-Season-4-Finale-Colorful-Shields.jpg"
+var imageUrl = "http://www.archeoroma.it/wp-content/uploads/2016/10/Colosseo-laptop_1040_529.jpeg"
 
 var headers = {
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': 'aecdcf236e59452499b41a84a8a56849'
+    'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY
 }
 var options = {
     url: 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description&language=en',
     method: 'POST',
     headers: headers,
-    json: { "url":"https://blogs-images.forbes.com/erikkain/files/2017/02/Vikings-Season-4-Finale-Colorful-Shields.jpg"}
+    json: { "url": imageUrl}
 }
 request(options, function (error, response, body) {
   if (!error && response.statusCode == 200) {
