3799f9871d331d3e29302d84ba75f7b2e37c5676
Giuseppe D'Aquanno <33717671+GiuseppeDaquanno@users.noreply.github.com>
2017-11-23 17:23:15 +0100

    Delete auth.js

--- middlewares/auth.js
+++
@@ -1,4 +0,0 @@
-//Marco questo è il tuo file, io di Oauth ancora non so niente quindi se mai comincia a pensare a come
-//si può integrare questo file con il controller
-
-exports.authenticate = function() {}
