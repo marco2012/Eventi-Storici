7fa5edb581166d7e7c59575db4b316c151b77da7
marco2012 <marco.fox.marco@gmail.com>
2017-11-24 12:24:25 +0100

    youtue

--- youtube.js
+++ youtube.js
@@ -1,17 +1,17 @@
 //https://developers.google.com/youtube/v3/code_samples/javascript#search-by-keyword
-
+//https://www.npmjs.com/package/youtube-node
+var request = require('request');
 var YouTube = require('youtube-node');
+
 var youTube = new YouTube();
 var YOUTUBE_KEY = 'AIzaSyCgBUJdvcF7Cx5jA9qSMSsxRAwvT0zimOA'
-var query = 'Docker'
-
 youTube.setKey(YOUTUBE_KEY);
 
+var query = 'Docker'
 youTube.search(query, 2, function(error, result) {
-  if (error) {
-    console.log(error);
-  }
+  if (error) console.log(error);
   else {
-    console.log(JSON.stringify(result, null, 2));
+    var body = JSON.stringify(result, null, 2)
+    
   }
 });
