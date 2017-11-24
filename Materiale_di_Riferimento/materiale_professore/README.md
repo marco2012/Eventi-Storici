c3379b03dedfa696ee9f2000d0d0c93c4af673b5
marco2012 <marco.fox.marco@gmail.com>
2017-11-24 19:18:00 +0100

    materiale professore

---
+++ Materiale_di_Riferimento/materiale_professore/README.md
@@ -0,0 +1,24 @@
+# RC
+The purpose of this material is to provide a step-by-step overview of some of the main techniques employed in modern network application and services.
+
+## TX/RX data over a network: socket
+
+## C/S Protocols: HTTP
+
+## Application Server: [NodejS](https://nodejs.org/en/)
+
+## REST: [Express](https://expressjs.com/)
+
+## Authentication and Authorization: Oauth and Password
+
+## Messaging: AMQP
+
+## Microservices: Seneca
+
+## IoT
+
+### DEaling with constraints: COAP and MQTT
+
+## Data streaming: Kafka
+
+## PaaS: Dokku
--- middlewares/GoogleDriveOauth.js
+++ middlewares/GoogleDriveOauth.js
@@ -22,60 +22,58 @@ var oauthlink = "https://accounts.google.com/o/oauth2/auth?client_id="+client_id
 
 exports.getAccess = function(callback){
 
-app.get("/", function(req,res){                                             //redirect per la gestione del grant dei permessi (authorization request phase)
-  res.redirect(oauthlink);
-});
+    app.get("/", function(req,res){                                             //redirect per la gestione del grant dei permessi (authorization request phase)
+      res.redirect(oauthlink);
+    });
 
-app.get("/oauth2callback", function(req,res){
+    app.get("/oauth2callback", function(req,res){
 
-  //controllo errori
-  if (req.query.error){                                                   //gestione errori(accesso negato)
-    var errorHtml= path.resolve("../"+"/html/error.html");                    //restituisce il percorso completo di error.html presente nella stessa cartella
-    res.writeHead(200, {"Content-Type": "text/html"});
-    fs.createReadStream(errorHtml).pipe(res);
-    console.log("\t"+"Access denied!!");                                //debug
-    // res.send("RECEIVED ERROR " + req.query.error);
-    return;
-  }
-  //ottengo codice autenticazione
-  authCode = req.query.code;                                              //nella risposta al grant se non ci sono errori è presente l'authentication code all'interno di una query
-  console.log("\t"+"Authentication Code obtained: "+authCode);            //debug
+      //controllo errori
+      if (req.query.error){                                                   //gestione errori(accesso negato)
+        var errorHtml= path.resolve("../"+"/html/error.html");                    //restituisce il percorso completo di error.html presente nella stessa cartella
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.createReadStream(errorHtml).pipe(res);
+        console.log("\t"+"Access denied!!");                                //debug
+        // res.send("RECEIVED ERROR " + req.query.error);
+        return;
+      }
+      //ottengo codice autenticazione
+      authCode = req.query.code;                                              //nella risposta al grant se non ci sono errori è presente l'authentication code all'interno di una query
+      console.log("\t"+"Authentication Code obtained: "+authCode);            //debug
 
-  //richiesta token
-  var accessToken;
-  var options = {
-    hostname: "www.googleapis.com",
-    port: 443,
-    method: 'POST',
-    path: "/oauth2/v4/token?code="+authCode+"&client_id="+client_id+"&client_secret="+client_secret+"&redirect_uri="+redirect_uri+"&grant_type=authorization_code"
-  };
-  var replyToken ="";
-  var reqAccess = https.request(options, function(resStream){                 //connessione sicura verso l'authorization server di google (auth grant phase)
-  resStream.setEncoding("utf8");
-  resStream.on("data", function (pezzo) {
-    replyToken += pezzo;
-  });
+      //richiesta token
+      var accessToken;
+      var options = {
+        hostname: "www.googleapis.com",
+        port: 443,
+        method: 'POST',
+        path: "/oauth2/v4/token?code="+authCode+"&client_id="+client_id+"&client_secret="+client_secret+"&redirect_uri="+redirect_uri+"&grant_type=authorization_code"
+      };
+      var replyToken ="";
+      var reqAccess = https.request(options, function(resStream){                 //connessione sicura verso l'authorization server di google (auth grant phase)
+      resStream.setEncoding("utf8");
+      resStream.on("data", function (pezzo) {
+        replyToken += pezzo;
+      });
 
-  resStream.on("end", function () {
-    replyToken=JSON.parse(replyToken);
-    accessToken = replyToken.access_token;                              //ottenuto Access Token
-    console.log("\t"+"Access Token obtained: "+accessToken); 	        //debug
+      resStream.on("end", function () {
+        replyToken=JSON.parse(replyToken);
+        accessToken = replyToken.access_token;                              //ottenuto Access Token
+        console.log("\t"+"Access Token obtained: "+accessToken); 	        //debug
 
+        var successHtml= path.resolve("../"+"/html/success.html");              //apro un file html con l'esito positivo dell'autenticazone
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.createReadStream(successHtml).pipe(res);
 
-    var successHtml= path.resolve("../"+"/html/success.html");              //apro un file html con l'esito positivo dell'autenticazone
-    res.writeHead(200, {"Content-Type": "text/html"});
-    fs.createReadStream(successHtml).pipe(res);
+        callback(accessToken);
+      });
+    });
 
+    reqAccess.end();
+    });
 
-    // callback(accessToken);
-  });
-});
-
-reqAccess.end();
-});
-
-app.listen(port, hostname, function(){
-  console.log("Server running at: "+hostname+":"+port+"\n");
-});
+    app.listen(port, hostname, function(){
+      console.log("Server running at: "+hostname+":"+port+"\n");
+    });
 
 };
