4176db2b429b648fecff0251d38f1a42a660e717
Francesco Gaudiello <Francesco@AirdiFrancesco-001.homenet.telecomitalia.it>
2017-11-26 16:36:09 +0100

    Provo a collegarci Youtube

--- ComeCazzoFunziona.js
+++ ComeCazzoFunziona.js
@@ -34,18 +34,20 @@ request.get(options, function callback(error, response, body) {
       var coll = info.data.Events[i].links;
       console.log(info.data.Events[i].links);
 
+
       //Nel caso li volessi singolarmente:
       for(var kk = 0; kk < info.data.Events[i].links.length; kk++){
-
         console.log("######################");
-        //Nel caso volessi solo il "title" dei links:
         console.log(info.data.Events[i].links[kk].title);
-        //Nel caso volessi solo il "link" wikipedia:
         console.log(info.data.Events[i].links[kk].link);
-
         //Se decidiamo di voler prendere solo il primo personaggio..
         //.. basta sostituire [0] nell'array "links"
-      }
     }
+
+    //Se voglio cercare il PRIMO TITLE di ogni anno SU YouTube
+    console.log("######################");
+    console.log("Cerco su Youtube: " + info.data.Events[i].links[0].title);
+
   }
+}
 });
