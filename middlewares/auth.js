ed560a5b162269b16581355e70cc1d5734b7068d
marco2012 <marco.fox.marco@gmail.com>
2017-11-28 10:42:38 +0200

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,9 @@
 ////////////////////////////////////////
 /////////////AUTENTICAZIONE/////////////
 ////////////////////////////////////////
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
 var CLIENT_ID = "282478069354-segjrssj65eartbgvvn212iqftjbfivj.apps.googleusercontent.com"
 var CLIENT_SECRET = "34cy0OjwgotDTtnFTkUOE0Hw"
 var REDIRECT_URL = "https://developers.google.com/oauthplayground"
@@ -8,11 +11,11 @@ var REFRESH_TOKEN = "1/fMRTq-Np0_qlXRWqRdTZloX-LpFLItzjEYuSikxGfeQ"
 
 exports.authenticate = function(callback){
 
-var auth = new googleAuth();
-var oauth2Client = new auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-oauth2Client.credentials = { refresh_token: REFRESH_TOKEN }; //auth
-google.options({ auth: oauth2Client }); // set auth as a global default
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+    oauth2Client.credentials = { refresh_token: REFRESH_TOKEN }; //auth
+    google.options({ auth: oauth2Client }); // set auth as a global default
 
-callback(oauth2Client)
+    callback(oauth2Client)
 
 }
