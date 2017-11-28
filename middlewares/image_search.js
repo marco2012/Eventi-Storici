f454cc5235d44740a7c87a85cd9f03f43fb66219
marco2012 <marco.fox.marco@gmail.com>
2017-11-28 09:18:27 +0200

    image

--- middlewares/image_search.js
+++ middlewares/image_search.js
@@ -1,4 +1,5 @@
 var request = require('request')
 
-var query = ''
-var url = 'https://api.qwant.com/api/search/images?count=10&offset=1&q='+query
+var query = ''  //parola da cercare
+var n = 1       //numero di risultati da mostrare per ricerca
+var url = 'https://api.qwant.com/api/search/images?count='+n+'&offset=1&q='+query
