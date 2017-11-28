01dfdd913d3d001d2bf17ff077874e45a8de3138
Francesca Fraioli <francesca@MacBook-Pro-di-Francesca.local>
2017-11-28 12:42:11 +0100

    Implementata: -scrittura su file -caricamento file su google drive

---
+++ drive/image.js
@@ -0,0 +1,37 @@
+var request = require('request');
+
+var n = 1;
+var query = ''
+
+
+var options = {
+    url : 'https://api.qwant.com/api/search/images?count=5&offset=1&q=gatti'
+}
+
+request.get(options, function callback(body) {
+        var info = JSON.parse(body);
+
+        console.log(body);
+        /*for (var i = 0; i < info.data.result.items.length; i++) {
+            console.log(info[1].data.result.items[0].media)
+        }*/
+
+});
+
+
+
+
+
+
+
+
+
+/*
+request.get(options, function callback(body){
+    var info = JSON.parse(body);
+    for(i = 0; i < info.data.result.lenght; i++){
+        var image = info.data.result[i].items[2];
+        console.log(image);
+    }
+});
+*/
