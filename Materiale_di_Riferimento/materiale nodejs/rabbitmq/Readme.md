97a2bef120865e88046ed3939f977675e5681808
marco2012 <marco.fox.marco@gmail.com>
2017-12-08 09:49:37 +0100

    pulizia

--- Materiale_di_Riferimento/materiale nodejs/rabbitmq/Readme.md
+++
@@ -1,4 +0,0 @@
-```
-docker run -d --hostname my-rabbit -p 5672:5672 --name some-rabbit rabbitmq:3
-```
-
--- Materiale_di_Riferimento/materiale nodejs/websocket/websocket.js
+++
@@ -1,9 +0,0 @@
-const WebSocket = require('ws');
-
-const wss = new WebSocket(port:9998);
-
-wss.on('connection', function connection(ws)) {
-	ws.on('message', function incoming(message) {
-		console.log('received: %s',message)
-	}
-} 
