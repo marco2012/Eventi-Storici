08db671901e17712eebfbf15b660f6726c10d1b8
marco2012 <marco.fox.marco@gmail.com>
2018-01-09 23:03:05 +0100

    fix youtube quando non trova link

--- middlewares/youtube.js
+++ middlewares/youtube.js
@@ -7,27 +7,27 @@ youTube.setKey(YOUTUBE_KEY);
 
 exports.searchVideo = function(query, callback){
     youTube.search(query, 1, function (error, result) { //cerco su youtube il titolo del primo link
-        if (error) console.log(error);
-        else {
-            var youtube_link
-            // if (result.items.length > 0) { //c'e' almeno 1 risultato
-            if (result.items != null) {  //l'array dei risultati esiste
-            var id = result.items[0].id.videoId
-            // if (id) {
-            youtube_link = 'https://www.youtube.com/watch?v='+id
-            console.log("[MIDDLEWARE][YOUTUBE]   Video su youtube trovato: "+youtube_link);
-            ws.send('[MIDDLEWARE][YOUTUBE]   <a target="_blank" href="'+youtube_link+'">Video su youtube trovato</a>')
-            // }
-            // else {
-            //     youtube_link = ''
-            //     console.log("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato");
-            //     ws.send("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato")
-            // }
+        if (error) {
+            console.log(error);
         } else {
-            console.log("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato");
-            ws.send("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato")
+            var youtube_link
+            if (result.items.length > 0) { //c'e' almeno 1 risultato
+                var id = result.items[0].id.videoId
+                if (id) {
+                    youtube_link = 'https://www.youtube.com/watch?v='+id
+                    console.log("[MIDDLEWARE][YOUTUBE]   Video su youtube trovato: "+youtube_link);
+                    ws.send('[MIDDLEWARE][YOUTUBE]   <a target="_blank" href="'+youtube_link+'">Video su youtube trovato</a>')
+                }
+                else {
+                    youtube_link = ''
+                    console.log("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato");
+                    ws.send("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato")
+                }
+            } else {
+                console.log("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato");
+                ws.send("[MIDDLEWARE][YOUTUBE]   Video su youtube NON trovato")
+            }
+            callback(youtube_link)
         }
-        callback(youtube_link)
-    }
-})
+    })
 }
